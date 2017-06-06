43eba005e7310fb71eeb32f21a66b77aefbbfa48
Juan Rondon <juanrondonc@outlook.com>
2017-06-06 12:08:53 +1000

    fixed index.js issues

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,6 @@ socket.on('disconnect', function () {
   console.log('disconnected from server');
 });
 
-socket.on('newMessage', function (message) {
+socket.on('createMessage', function (message) {
   console.log('new Message', message);
 });
--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('new user connected');  
+  console.log('new user connected');
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
